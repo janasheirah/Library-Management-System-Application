d25f0f1866daee39a123002fa5d1671aa569504e
janaayman <janayman@student.ubc.ca>
2022-11-14 00:10:46 -0800

    fixing autobot compiler issue

--- src/main/ui/LibraryUI.java
+++ src/main/ui/LibraryUI.java
@@ -7,11 +7,8 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
